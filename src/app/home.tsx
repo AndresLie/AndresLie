e648245c5134cb830a7b4f7c9a3349a12d7ed245
Eroffff <andreeli707@gmail.com>
2024-09-02 22:08:59 +0800

    change ' with &apos;

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -8,11 +8,11 @@ export function Home() {
   return (
     <div className="h-[100vh] rounded-md bg-neutral-900 flex flex-col items-center justify-center relative w-full">
       <h2 className="relative flex-col md:flex-row z-10 text-3xl md:text-5xl md:leading-tight max-w-5xl mx-auto text-center tracking-tight font-medium bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-white to-white flex items-center gap-2 md:gap-8">
-            Hi, I'm Andreas<br />
+            Hi, I&apos;m Andreas<br />
       </h2>
     <div>
         <h2 className="relative flex-col md:flex-row z-10 text-3xl md:text-5xl md:leading-tight max-w-5xl mx-auto text-center tracking-tight font-medium bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-white to-white flex items-center gap-2 md:gap-8">
-            I'm a<FlipWords words={words} className="text-neutral-100 z-99" />   
+            I&apos;m a<FlipWords words={words} className="text-neutral-100 z-99" />   
         </h2>
     </div>
 
